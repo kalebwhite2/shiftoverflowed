46458ffa2d0461cdc162bc53f57f12bc163b2aed
Kaleb White <kalebw02@ad.unc.edu>
2024-03-02 00:45:47 -0500

    Finish functionality for first row of team view

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -28,12 +28,31 @@ class Admin::TeamsController < Admin::BaseController
     # Here, each will be assigned to each parameter in a way that resembles in an array of arrays
     # ie, for team_name, p will look like ["team_name", "new_team_name"]
     params.each do |p|
-      if Team.column_names.include? p[0]
-        @team.update_attribute(p[0], p[1])
+      # We'll use a switch statement to decide how to update @team
+      logger.info(p)
+      case p[0]
+      when "shift_types"
+        # Split splits the string into an array using the delimiting string ", "
+        p[1].split(", ").each do |new_shift_type_name|
+          # The << symbol assosciates the new shift_type with this @team
+          @team.shift_types << ShiftType.new(name: new_shift_type_name)
+          flash[:notice] = "Added shift type #{p[1]} to the team."
+        end
+      when "remove_shift_type"
+        # Exclude default value none
+        logger.info(p[1])
+        if p[1] != "None" and @team.shift_types.find_by(name: p[1]).destroy
+          flash[:notice] = "Removed shift type #{p[1]} from the team."
+        end
+      else
+        # Exclude default value id
+        if p[0] != "id" and Team.column_names.include? p[0]
+          @team.update_attribute(p[0], p[1])
+          flash[:notice] = "Set team #{p[0]} to #{p[1]}."
+        end
       end
     end
     if @team.save
-      flash[:notice] = "Succesfully updated team!"
     else
       flash[:alert] = "Failed to update team."
     end
